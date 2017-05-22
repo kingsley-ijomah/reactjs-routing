ab5443929bb8b004387120c7b219cda32fd2cef1
kingsley ijomah <kingsley.ijomah@gmail.com>
2017-05-22 17:03:42 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Navigation from './Components/Navigation';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Navigation />
+    );
+  }
+}
+
+export default App;
